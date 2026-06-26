290c09f8e4995fceb612cadc8d9c24a5fba085d8
AAKASH G <aakashvkl4@gmail.com>
2026-06-26 09:42:23 +0530

    style: fix flake8 formatting issues in tests

--- tests/test_threat_scoring.py
+++ tests/test_threat_scoring.py
@@ -1,20 +1,23 @@
 import pytest
 from analytics.threat_scoring import ThreatScoringEngine
 
+
 def test_threat_scoring_addition():
     engine = ThreatScoringEngine()
     engine.add_offense("10.0.0.1", "block")
     assert engine.scores["10.0.0.1"] == 10
-    
+
     engine.add_offense("10.0.0.1", "port_scan")
     assert engine.scores["10.0.0.1"] == 35
+
 
 def test_threat_scoring_cap():
     engine = ThreatScoringEngine()
     for _ in range(5):
         engine.add_offense("10.0.0.1", "brute_force")
-        
+
     assert engine.scores["10.0.0.1"] == 100.0  # Should be capped
+
 
 def test_threat_classification():
     engine = ThreatScoringEngine()
@@ -23,10 +26,11 @@ def test_threat_classification():
     assert engine.get_classification(60) == "Dangerous"
     assert engine.get_classification(90) == "Critical"
 
+
 def test_decay():
     engine = ThreatScoringEngine()
     engine.add_offense("10.0.0.2", "block")
     assert engine.scores["10.0.0.2"] == 10
-    
+
     engine.decay_scores()
     assert engine.scores["10.0.0.2"] < 10
